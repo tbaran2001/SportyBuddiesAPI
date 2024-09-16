7fa8c32f58daa23e27bd157996c01697105e6666
tbaran2001 <barantomasz2001@gmail.com>
2024-09-16 21:18:32 +0200

    Implement integrations tests

---
+++ SportyBuddies.Api.IntegrationTests/Common/SportyBuddiesApiCollection.cs
@@ -0,0 +1,7 @@
+﻿namespace SportyBuddies.Api.IntegrationTests.Common;
+
+[CollectionDefinition(CollectionName)]
+public class SportyBuddiesApiCollection : ICollectionFixture<SportyBuddiesApiFactory>
+{
+    public const string CollectionName = "SportyBuddiesApiFactoryCollection";
+}
---
+++ SportyBuddies.Api.IntegrationTests/Controllers/SportsController.CreateSportTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Shouldly;
+using SportyBuddies.Api.IntegrationTests.Common;
+using SportyBuddies.Contracts.Sports;
+
+namespace SportyBuddies.Api.IntegrationTests.Controllers;
+
+[Collection(SportyBuddiesApiCollection.CollectionName)]
+public class CreateSubscriptionTests
+{
+    private readonly HttpClient _client;
+
+    public CreateSubscriptionTests(SportyBuddiesApiFactory apiFactory)
+    {
+        _client = apiFactory.HttpClient;
+        apiFactory.ResetDatabase();
+    }
+
+    [Fact]
+    public async Task CreateSport_WhenValidSport_ShouldCreateSport()
+    {
+        // Arrange
+        var createSportRequest = new CreateSportRequest(
+            "Football",
+            "Football description");
+
+        // Act
+        var response = await _client.PostAsJsonAsync("api/Sports", createSportRequest);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+        response.Headers.Location.ShouldNotBeNull();
+
+        var sportResponse = await response.Content.ReadFromJsonAsync<SportResponse>();
+        sportResponse.ShouldNotBeNull();
+        sportResponse!.Name.ShouldBe("Football");
+
+        response.Headers.Location!.PathAndQuery.ShouldBe($"/api/Sports/{sportResponse.Id}");
+    }
+}
