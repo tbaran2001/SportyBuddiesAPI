5d07e480293202eb063e9ba63685a03da1c30a1f
Tomasz Baran <barantomasz2001@gmail.com>
2024-10-26 17:14:15 +0200

    validator and db

--- SportyBuddies.Application/Users/Commands/UpdateUser/UpdateUserCommandValidator.cs
+++ SportyBuddies.Application/Users/Commands/UpdateUser/UpdateUserCommandValidator.cs
@@ -7,8 +7,19 @@ public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
 {
     public UpdateUserCommandValidator()
     {
-        RuleFor(x => x.Name).NotEmpty().MaximumLength(10);
-        RuleFor(x => x.Description).NotEmpty().MaximumLength(60);
-        RuleFor(x => x.Gender).Must(gender => Enum.IsDefined(typeof(Gender), gender) && gender != Gender.Unknown);
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(10);
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .MaximumLength(60);
+        RuleFor(x => x.Gender)
+            .Must(gender => Enum.IsDefined(typeof(Gender), gender) && gender != Gender.Unknown);
+        //make date of birth validation cant be less than 18 years old
+        RuleFor(x => x.DateOfBirth)
+            .NotEmpty()
+            .LessThan(DateTime.Now.AddYears(-18))
+            .GreaterThan(DateTime.Now.AddYears(-100));
+            
     }
 }
