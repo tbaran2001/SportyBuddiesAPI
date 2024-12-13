6704667dd984b26a43f1b3d21c57c7122d509ce4
Tomasz Baran <barantomasz2001@gmail.com>
2024-12-13 23:49:25 +0100

    Add health checks

--- microservices/Services/Sport/Sport.API/Program.cs
+++ microservices/Services/Sport/Sport.API/Program.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Sport.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +10,8 @@ builder.Services.AddCarter();
 builder.Services.AddMarten(options => { options.Connection(builder.Configuration.GetConnectionString("Database")!); })
     .UseLightweightSessions();
 
-if (builder.Environment.IsDevelopment())
-    builder.Services.InitializeMartenWith<SportInitialData>();
+/*if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<SportInitialData>();*/
 
 var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(configuration =>
@@ -22,10 +24,19 @@ builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
